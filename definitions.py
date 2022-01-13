6742ab0d4d9cff03db404b23b7d6022c440a1625
Jannis Baum <jannis.baum@gmail.com>
2022-01-13 11:54:39 +0100

    fix path to allow running form outside of root dir

--- definitions.py
+++ definitions.py
@@ -9,6 +9,6 @@ CMD_QL = os.getenv('CMD_QUICKLOOK', 'open')
 
 QL_DEF = os.getenv('QUICKLOOK_BY_DEFAULT', False) in ['true', 1, 't', 'True', 'T']
 
-TEMPLATE_DIR = os.getenv('TEMPLATE_DIR', 'templates')
+TEMPLATE_DIR = os.path.join(ROOT_DIR, os.getenv('TEMPLATE_DIR', 'templates'))
 TEMPLATE_RECENT = os.getenv('TEMPLATE_RECENT', '_recent') + '.yaml'
 
