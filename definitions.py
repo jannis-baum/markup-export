ca2a08da8d80907559a0196587b0b5a783ba94fd
Jannis Baum <jannis.baum@gmail.com>
2022-01-13 12:01:45 +0100

    commands with format placeholder

--- definitions.py
+++ definitions.py
@@ -4,8 +4,8 @@ ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 from dotenv import load_dotenv
 load_dotenv()
 
-CMD_EDITOR = os.getenv('CMD_EDITOR', 'vim')
-CMD_QL = os.getenv('CMD_QUICKLOOK', 'open')
+CMD_EDITOR = os.getenv('CMD_EDITOR', 'vim {}')
+CMD_QL = os.getenv('CMD_QUICKLOOK', 'open {}')
 
 QL_DEF = os.getenv('QUICKLOOK_BY_DEFAULT', False) in ['true', 1, 't', 'True', 'T']
 
